86a8de2007d1f31b1a92a9263808d3d52f6d938c
Nourath <nouraffo08@email.com>
2025-12-25 12:24:47 +0100

    Mise à jour des liens de téléchargement du CV et ajout du nouveau CV

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -103,11 +103,11 @@ function About() {
 
           <div className="pt-6">
     <a 
-        href="/cv.pdf" 
-        download="CV_Nourath_AFFO.pdf" 
-        className="bg-[#7cf7ad] hover:bg-[#6be69d] text-black px-8 py-4 rounded-lg font-semibold inline-flex items-center gap-2 transition-all shadow-lg shadow-[#7cf7ad]/20 hover:-translate-y-1"
+      href="/Cv%20Nourath%20AFFO%20.pdf" 
+      download="Cv_Nourath_AFFO.pdf" 
+      className="bg-[#7cf7ad] hover:bg-[#6be69d] text-black px-8 py-4 rounded-lg font-semibold inline-flex items-center gap-2 transition-all shadow-lg shadow-[#7cf7ad]/20 hover:-translate-y-1"
     >
-        Téléchargez mon CV <Download size={20} />
+      Téléchargez mon CV <Download size={20} />
     </a>
 </div>
         </div>
